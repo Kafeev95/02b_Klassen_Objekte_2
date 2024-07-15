78b230ed3466b0e498ce2a1d1ef0501ecad54e56
Kafeev95 <andre.ms@web.de>
2024-07-15 12:32:28 +0200

    01/ Felder1. Iteration

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,17 @@ package com.cc.java;
 
 public class Cat 
 {
+    // Eigenschaften / properties
+    // Felder / fields / attributes
+
+    // pass by reference
+    String name = "Grizabella";
+    String furColor = "white";
+
+    // pass by value
+    int age = 29;
+
+
     // Rückgabetyp -- Cat
     public Cat getInstanceVariable(){
         return this;
